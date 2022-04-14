cef23499b341e565865b43e1f7276974a806d149
TeddyH08 <teddy.hemonet@gmail.com>
2022-04-14 10:44:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Covid Tracker</title>
+
+        <link rel="stylesheet" href="assets/style/style.css">
+    </head>
+
+    <body>
+        <header>
+            <div class="g">
+                <h1>Covid Tracker</h1>
+                <p>Bienvenue sur Covid Tracker, ici vous verrez toutes les statistiques recenssés à propos de la covid.</p>
+            </div>
+
+            <div class="d">
+                <h2>Nombre de cas :</h2>
+                <p></p>
+                <h2>Nombre de décès :</h2>
+                <p></p>
+            </div>
+        </header>
+
+
+    </body>
+</html>
